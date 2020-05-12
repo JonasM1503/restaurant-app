5e1282c37a80127c42e6b6d941be3ff335668a86
SimonRothmann <43773218+SimonRothmann@users.noreply.github.com>
2020-05-12 21:16:55 +0200

    registration finished

--- app/src/main/java/com/example/restaurant_app/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/restaurant_app/activities/RegistrationActivity.java
@@ -1,8 +1,10 @@
 package com.example.restaurant_app.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,21 +34,22 @@ public class RegistrationActivity extends AppCompatActivity {
         userManager = UserFirestoreManager.newInstance();
         addressManager = AddressFirestoreManager.newInstance();
 
-        final Button restigsterButton = findViewById(R.id.finishRegistration);
-        final String firstName = findViewById(R.id.registerFirstName).toString();
-        final String lastName = findViewById(R.id.registerLastName).toString();
-        final String email = findViewById(R.id.registerEmail).toString();
-        final String password = findViewById(R.id.registerPassword).toString();
-        final String street = findViewById(R.id.registerStreet).toString();
-        final String zipCode = findViewById(R.id.registerZipCode).toString();
-        final String city = findViewById(R.id.registerCity).toString();
-        final String restaurantName = findViewById(R.id.registerRestaurantName).toString();
-        final String ustId = findViewById(R.id.registerUstId).toString();
+        final Button registerButton = findViewById(R.id.finishRegistration);
 
 
-        restigsterButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String firstName = ((TextView) findViewById(R.id.registerFirstName)).getText().toString();
+                final String lastName = ((TextView) findViewById(R.id.registerLastName)).getText().toString();
+                final String email = ((TextView) findViewById(R.id.registerEmail)).getText().toString();
+                final String password = ((TextView) findViewById(R.id.registerPassword)).getText().toString();
+                final String street = ((TextView) findViewById(R.id.registerStreet)).getText().toString();
+                final String zipCode = ((TextView) findViewById(R.id.registerZipCode)).getText().toString();
+                final String city = ((TextView) findViewById(R.id.registerCity)).getText().toString();
+                final String restaurantName = ((TextView) findViewById(R.id.registerRestaurantName)).getText().toString();
+                final String ustId = ((TextView) findViewById(R.id.registerUstId)).getText().toString();
+
                 Address address =  new Address(street, zipCode, city, "Germany");
                 Restaurant restaurant = new Restaurant(restaurantName, address, ustId);
                 User user = new User(email, password, firstName, lastName, address, false, restaurant);
@@ -54,7 +57,9 @@ public class RegistrationActivity extends AppCompatActivity {
                 resManager.createRestaurant(restaurant);
                 addressManager.createAddress(address);
                 userManager.createUser(user);
-                setContentView(R.layout.activity_main);
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                v.getContext().startActivity(intent);
+
             }
         });
     }
