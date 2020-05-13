9c63652b827278dbb8519331341f6c1e4d4a00d0
JonasM1503 <jonasmitschke@hotmail.de>
2020-05-13 15:26:57 +0200

    added password hashing

--- app/src/main/java/com/example/restaurant_app/models/User.java
+++ app/src/main/java/com/example/restaurant_app/models/User.java
@@ -1,9 +1,14 @@
 package com.example.restaurant_app.models;
 
 import com.google.firebase.firestore.DocumentId;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  *
- * @author Simon Rothmann
+ * @author Simon Rothmann, Jonas Mitschke
  * @content definition of user-class
  */
 public class User {
@@ -16,6 +21,7 @@ public class User {
     private Address address;
     private Boolean isActive;
     private Restaurant restaurant;
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
     //constructor
     public User(String email, String password, String firstName, String lastName,
@@ -54,7 +60,7 @@ public class User {
     //setter
     public void setEmail(String email) { this.email = email; }
 
-    public void setPassword(String password) { this.password = password; }
+    public void setPassword(String password) { this.password = this.hashPassword(password); }
 
     public void setFirstName(String firstName) { this.firstName = firstName; }
 
@@ -65,4 +71,32 @@ public class User {
     public void setAdmin(Boolean admin) { isActive = admin; }
 
     public void setRestaurant(Restaurant restaurant) { this.restaurant = restaurant; }
+
+    // hash password
+    public static String hashPassword(String password) {
+        byte[] hash;
+
+        if (password == null) {
+            password = "";
+        }
+
+        // Hashwert zum Passwort berechnen
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException ex) {
+            hash = "!".getBytes(StandardCharsets.UTF_8);
+        }
+
+        // Hashwert in einen Hex-String umwandeln
+        char[] hashHex = new char[hash.length * 2];
+
+        for (int i = 0; i < hash.length; i++) {
+            int v = hash[i] & 0xFF;
+            hashHex[i * 2] = HEX_ARRAY[v >>> 4];
+            hashHex[i * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+
+        return new String(hashHex);
+    }
 }
