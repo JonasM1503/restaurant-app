9c67c98eeb26b16d12fec206693529c59c27124a
JonasM1503 <jonasmitschke@hotmail.de>
2020-06-15 10:54:34 +0200

    moved query to FirestoreManager & added errorhandling

--- app/src/main/java/com/example/restaurant_app/activities/MenuActivity.java
+++ app/src/main/java/com/example/restaurant_app/activities/MenuActivity.java
@@ -1,34 +1,26 @@
 package com.example.restaurant_app.activities;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.restaurant_app.adapters.FoodListAdapter;
 import com.example.restaurant_app.adapters.MenuDrinkListAdapter;
 import com.example.restaurant_app.adapters.MenuFoodListAdapter;
 import com.example.restaurant_app.adapters.SharedPreferencesAdapter;
 import com.example.restaurant_app.firestore.DrinkFirestoreManager;
 import com.example.restaurant_app.firestore.FoodFirestoreManager;
 import com.example.restaurant_app.firestore.TableFirestoreManager;
-import com.example.restaurant_app.helpers.CollectionNames;
 import com.example.restaurant_app.models.Drink;
 import com.example.restaurant_app.models.Food;
 import com.example.restaurant_app.models.Table;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,78 +29,81 @@ import java.util.List;
  * @content activity for menu-page
  */
 public class MenuActivity extends AppCompatActivity {
+
     private TableFirestoreManager tableFirestoreManager;
-    private static Button foodButton;
-    private static Button drinkButton;
+    private FoodFirestoreManager foodFirestoreManager;
+    private DrinkFirestoreManager drinkFirestoreManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_page);
 
+        final Activity this_activity = this;
         Context context = getBaseContext();
         final String tableID = SharedPreferencesAdapter.getDefaults("currentTable", context);
+
         tableFirestoreManager = TableFirestoreManager.newInstance();
-        final Table[] table = new Table[1];
+        foodFirestoreManager = FoodFirestoreManager.newInstance();
+        drinkFirestoreManager = DrinkFirestoreManager.newInstance();
+
         tableFirestoreManager.getTableById(tableID, new TableFirestoreManager.GetTableByIdCallback() {
             @Override
-            public void onCallback(Table tableCallback) {
-                table[0] = tableCallback;
+            public void onCallback(Table table) {
+                final Button foodButton = findViewById(R.id.menuFoodButton);
+                foodButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        foodFirestoreManager.getFoodsByRestaurantId(table.getRestaurantId(), new FoodFirestoreManager.GetFoodsByRestaurantCallback() {
+                            @Override
+                            public void onCallback(List<Food> foods) {
+                                if (foods.size() > 0) {
+                                    ListView menuListView = findViewById(R.id.menuListView);
+                                    MenuFoodListAdapter foodAdapter = new MenuFoodListAdapter(MenuActivity.this, foods);
+                                    menuListView.setAdapter(foodAdapter);
+                                }
+                            }
+
+                            @Override
+                            public void onFailureCallback(Exception e) {
+                                Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_error), Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        });
+                    }
+                });
+
+                final Button drinkButton = findViewById(R.id.menuDrinkButton);
+                drinkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        drinkFirestoreManager.getDrinksByRestaurantId(table.getRestaurantId(), new DrinkFirestoreManager.GetDrinksByRestaurantCallback() {
+                            @Override
+                            public void onCallback(List<Drink> drinks) {
+                                if (drinks.size() > 0) {
+                                    ListView menuListView = findViewById(R.id.menuListView);
+                                    MenuDrinkListAdapter drinkAdapter = new MenuDrinkListAdapter(MenuActivity.this, drinks);
+                                    menuListView.setAdapter(drinkAdapter);
+                                }
+                            }
+
+                            @Override
+                            public void onFailureCallback(Exception e) {
+                                Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_error), Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        });
+                    }
+                });
             }
 
             @Override
             public void onFailureCallback(Exception e) {
-
+                Intent intent = new Intent(this_activity, MainActivity.class);
+                this_activity.startActivity(intent);
+                Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_error), Toast.LENGTH_LONG);
+                toast.show();
             }
         });
-
-        foodButton = findViewById(R.id.menuFoodButton);
-        foodButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseFirestore.getInstance().collection(CollectionNames.foodCollection)
-                        .whereEqualTo("restaurantId", table[0].getRestaurantId()).get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                List<Food> return_foods = new ArrayList<>();
-                                if(task.isSuccessful()){
-                                    for(DocumentSnapshot document : task.getResult()) {
-                                        Food food = document.toObject(Food.class);
-                                        return_foods.add(food);
-                                    }
-                                    ListView menuListView = findViewById(R.id.menuListView);
-                                    MenuFoodListAdapter foodAdapter = new MenuFoodListAdapter(MenuActivity.this, return_foods);
-                                    menuListView.setAdapter(foodAdapter);
-                                }
-                            }
-                        });
-            }
-        });
-
-        drinkButton = findViewById(R.id.menuDrinkButton);
-        drinkButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseFirestore.getInstance().collection(CollectionNames.drinkCollection)
-                        .whereEqualTo("restaurantId", table[0].getRestaurantId()).get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                List<Drink> return_drinks = new ArrayList<>();
-                                if(task.isSuccessful()){
-                                    for(DocumentSnapshot document : task.getResult()) {
-                                        Drink drink = document.toObject(Drink.class);
-                                        return_drinks.add(drink);
-                                    }
-                                    ListView menuListView = findViewById(R.id.menuListView);
-                                    MenuDrinkListAdapter drinkAdapter = new MenuDrinkListAdapter(MenuActivity.this, return_drinks);
-                                    menuListView.setAdapter(drinkAdapter);
-                                }
-                            }
-                        });
-            }
-        });
-
     }
 }
