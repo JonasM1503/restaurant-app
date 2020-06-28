46453f71d8abcfb41ce25f2a55858c56f3d97b0a
JonasM1503 <jonasmitschke@hotmail.de>
2020-06-28 22:49:55 +0200

    changed address for email sending

--- app/src/main/java/com/example/restaurant_app/activities/ShopingCartActivity.java
+++ app/src/main/java/com/example/restaurant_app/activities/ShopingCartActivity.java
@@ -13,9 +13,13 @@ import android.widget.Toast;
 import com.example.restaurant_app.adapters.SharedPreferencesAdapter;
 import com.example.restaurant_app.adapters.ShopingCartAdapter;
 import com.example.restaurant_app.firestore.OrderFirestoreManager;
+import com.example.restaurant_app.firestore.TableFirestoreManager;
+import com.example.restaurant_app.firestore.UserFirestoreManager;
 import com.example.restaurant_app.helpers.MailSender;
 import com.example.restaurant_app.models.Order;
 import com.example.restaurant_app.models.OrderPos;
+import com.example.restaurant_app.models.Table;
+import com.example.restaurant_app.models.User;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -31,6 +35,8 @@ public class ShopingCartActivity extends Activity {
 
     private String msg_text;
     private OrderFirestoreManager orderFirestoreManager;
+    private TableFirestoreManager tableFirestoreManager;
+    private UserFirestoreManager userFirestoreManager;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,8 @@ public class ShopingCartActivity extends Activity {
        setContentView(R.layout.shopingcart_page);
 
        orderFirestoreManager = OrderFirestoreManager.newInstance();
+       tableFirestoreManager = TableFirestoreManager.newInstance();
+       userFirestoreManager = UserFirestoreManager.newInstance();
 
        Gson gson = new Gson();
        Context context = getBaseContext();
@@ -73,27 +81,47 @@ public class ShopingCartActivity extends Activity {
                    }
                }
 
-               try {
-                   MailSender sender = new MailSender();
-                   sender.sendMail(getResources().getString(R.string.email_subject),
-                           msg_text,
-                           "restaurant-app@jonasmitschke.com",
-                           "jmitschke2012@gmail.com");
-                   SharedPreferencesAdapter.setListDefaults("orderPosList", new ArrayList<String>(), context);
-               } catch (Exception e) {
-                   Log.e("SendMailError", e.getMessage(), e);
-               }
+               tableFirestoreManager.getTableById(tableID, new TableFirestoreManager.GetTableByIdCallback() {
+                   @Override
+                   public void onCallback(Table table) {
+                       userFirestoreManager.getUserByRestaurantId(table.getRestaurantId(), new UserFirestoreManager.GetUserByRestaurantCallback() {
+                           @Override
+                           public void onCallback(User user) {
+                               try {
+                                   MailSender sender = new MailSender();
+                                   sender.sendMail(getResources().getString(R.string.email_subject),
+                                           msg_text,
+                                           "restaurant-app@jonasmitschke.com",
+                                           "jmitschke2012@gmail.com" /* for live: user.getEmail() */);
+                                   SharedPreferencesAdapter.setListDefaults("orderPosList", new ArrayList<String>(), context);
+                               } catch (Exception e) {
+                                   Log.e("SendMailError", e.getMessage(), e);
+                               }
 
-               Order new_order = new Order(tableID, false, orderPosList);
-               orderFirestoreManager.createOrder(new_order);
+                               Order new_order = new Order(tableID, false, orderPosList);
+                               orderFirestoreManager.createOrder(new_order);
 
-               Intent intent = new Intent(view.getContext(), MenuActivity.class);
-               view.getContext().startActivity(intent);
-               Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_email_sent), Toast.LENGTH_LONG);
-               toast.show();
+                               Intent intent = new Intent(view.getContext(), MenuActivity.class);
+                               view.getContext().startActivity(intent);
+                               Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_email_sent), Toast.LENGTH_LONG);
+                               toast.show();
+                           }
+
+                           @Override
+                           public void onFailureCallback(Exception e) {
+                               Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_error), Toast.LENGTH_LONG);
+                               toast.show();
+                           }
+                       });
+                   }
+
+                   @Override
+                   public void onFailureCallback(Exception e) {
+                       Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_error), Toast.LENGTH_LONG);
+                       toast.show();
+                   }
+               });
            }
        });
-
    }
-
 }
--- app/src/main/java/com/example/restaurant_app/firestore/UserFirestoreManager.java
+++ app/src/main/java/com/example/restaurant_app/firestore/UserFirestoreManager.java
@@ -15,6 +15,9 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Simon Rothmann, Jonas Mitschke
@@ -78,6 +81,56 @@ public class UserFirestoreManager {
                 }
             }
         }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                callback.onFailureCallback(exception);
+            }
+        });
+    }
+
+    /**
+     *
+     * @author   Jonas Mitschke
+     * @content  interface to work with returned user from firestore
+     */
+    public interface GetUserByRestaurantCallback {
+        void onCallback(User user);
+        void onFailureCallback(Exception e);
+    }
+
+    /**
+     *
+     * @author   Jonas Mitschke
+     * @content  get user from firestore by restaurant
+     * @param    resId       id of the restaurant
+     * @param    callback    GetUserByRestaurantCallback-interface
+     */
+    public void getUserByRestaurantId(String resId, final UserFirestoreManager.GetUserByRestaurantCallback callback){
+        collectionReference.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot querySnapshot) {
+                        List<User> return_users = new ArrayList<>();
+
+                        for(DocumentSnapshot document : querySnapshot.getDocuments()) {
+                            User user = document.toObject(User.class);
+                            return_users.add(user);
+                        }
+
+                        User return_user = null;
+                        for (int i = 0; i < return_users.size(); i++){
+                            if (return_users.get(i).checkWhetherAdmin() == false && return_users.get(i).getRestaurant().getRestaurantId().equals(resId)) {
+                                return_user = return_users.get(i);
+                            }
+                        }
+
+                        if (return_user != null) {
+                            callback.onCallback(return_user);
+                        } else {
+                            callback.onFailureCallback(new Exception(new ClassNotFoundException()));
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
                 callback.onFailureCallback(exception);
